2c2ef7eb9e05982d9ccbe90ff3202ff18734a4c3
GonzaloDelbazi <gonzalodelbazi@gmail.com>
2023-12-13 17:27:11 -0300

    ahora si, primer commit JobbiApp

---
+++ App.tsx
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View style={{height: '100%', backgroundColor: 'lightblue', justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 50, fontWeight: '800', color: 'black'}}>JOBBI APP</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-JobbiApp-JobbiAppTests/Pods-JobbiApp-JobbiAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JobbiApp_JobbiAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_JobbiApp_JobbiAppTests
+@end

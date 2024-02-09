f97d7a17e72a89972bf03119e239bc862eaf11c1
GonzaloDelbazi <gonzalodelbazi@gmail.com>
2024-02-08 21:07:04 -0300

    archivo prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true, // Punto y coma al final de cada declaración
+  singleQuote: true, // Usa comillas simples en lugar de dobles
+  trailingComma: 'all', // Agrega una coma al final de los elementos en objetos y arrays
+  tabWidth: 2, // Tamaño de la tabulación
+  printWidth: 80, // Ancho máximo de la línea
+  endOfLine: 'auto', // Usa el estilo de fin de línea del sistema operativo
+}
